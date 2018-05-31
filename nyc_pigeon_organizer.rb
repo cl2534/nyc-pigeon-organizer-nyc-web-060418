cfd7c486ec5de46c582b73baefae3ba44e0b828b
Changmin Lim <chang.lim92@gmail.com>
2018-05-31 05:56:13 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result = {}
+  data.each do |ones, attributes|
++    attributes.each do |attribute, names|
++      names.each do |name|
++        if pigeons_organized[name] == nil
++          pigeons_organized[name]= {}
++        end
++        if pigeons_organized[name][ones] == nil
++          pigeons_organized[name][ones] = []
++          pigeons_organized[name][ones] << attribute.to_s
++        else
++          pigeons_organized[name][ones] << attribute.to_s
++        end
++      end
++    end
++  end
++  pigeons_organized
++end
 end
